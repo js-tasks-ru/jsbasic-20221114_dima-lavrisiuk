a34efd0840441d1170f573262e23fdf55799db85
Dima Lavrisiuk <dmitriyemigrant@gmail.com>
2022-12-02 03:59:25 +0300

    Убрал функцию

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,10 +1,8 @@
 function sumSalary(salaries) {
   let sum = 0;
 
-  const isNumber = value => Number.isFinite(value);
-
   for (const key in salaries) {
-    if (isNumber(salaries[key])) {
+    if (Number.isFinite(salaries[key])) {
       sum += salaries[key];
     }
   }
