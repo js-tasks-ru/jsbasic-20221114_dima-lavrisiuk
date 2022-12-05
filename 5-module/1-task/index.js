2c93ec7005224ffbd69b158993b5eb2108eac469
Dima Lavrisiuk <dmitriyemigrant@gmail.com>
2022-12-06 01:15:12 +0300

    Module 5 / Task 1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,7 @@
 function hideSelf() {
-  // ваш код...
+  document
+    .querySelector('.hide-self-button')
+    .addEventListener('click', (event) => {
+      event.target.hidden = true;
+  }, { once: true });
 }
