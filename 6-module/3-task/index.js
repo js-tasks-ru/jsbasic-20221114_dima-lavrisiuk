21fda7a947d130f6c283a845e5f684afd8b79f30
Dima Lavrisiuk <dmitriyemigrant@gmail.com>
2022-12-14 02:14:48 +0300

    Module 6 / Task 3 Done

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,93 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+  #carousel           = null;
+  #arrowLeft          = null;
+  #arrowRight         = null;
+  #currentSlideNumber = null;
+  #totalSlideAmount   = null;
+
   constructor(slides) {
-    this.slides = slides;
+    this.#carousel = createElement(this.#templateCarousel(slides));
+    this.#arrowLeft = this.#carousel.querySelector('.carousel__arrow_left');
+    this.#arrowRight = this.#carousel.querySelector('.carousel__arrow_right');
+    this.#totalSlideAmount = slides.length;
+
+    this.#setCurrentSlideNumber(0);
+
+    this.#arrowLeft.addEventListener('click', this.#onArrowClick);
+    this.#arrowRight.addEventListener('click', this.#onArrowClick);
+    this.#carousel.querySelectorAll('.carousel__slide').forEach(slide => {
+      slide.querySelector('.carousel__button').addEventListener('click', this.#onButtonClick);
+    });
+  }
+
+  get elem() {
+    return this.#carousel;
+  }
+
+  #onArrowClick = (event) => {
+    const direction = (event.currentTarget === this.#arrowLeft) ? -1 : +1;
+    this.#setCurrentSlideNumber(this.#currentSlideNumber + direction);
+  };
+
+  #onButtonClick = (event) => {
+    event.target.dispatchEvent(
+      new CustomEvent('product-add', {
+        bubbles: true,
+        detail: event.currentTarget.closest('.carousel__slide').dataset.id
+      })
+    );
+  };
+
+  #setCurrentSlideNumber(number) {
+    this.#currentSlideNumber = number;
+    const shift = this.#getSlideWidth() * this.#currentSlideNumber * -1;
+
+    this.#carousel.querySelector('.carousel__inner').style.transform = `translateX(${shift}px)`;
+    this.#updateVisibilityOfArrows();
+  }
+
+  #getSlideWidth() {
+    return this.#carousel.offsetWidth;
+  }
+
+  #updateVisibilityOfArrows() {
+    this.#arrowRight.style.display =
+      this.#currentSlideNumber === this.#totalSlideAmount - 1 ? 'none' : '';
+
+    this.#arrowLeft.style.display =
+      this.#currentSlideNumber === 0 ? 'none' : '';
+  }
+
+  #templateCarousel(slides) {
+    return `
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__inner">
+            ${slides.map((slide) => this.#templateSlide(slide)).join('')}
+        </div>
+      </div>
+    `;
+  }
+
+  #templateSlide(slide) {
+    return `
+      <div class="carousel__slide" data-id="${slide.id}">
+        <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">${'€' + slide.price.toFixed(2)}</span>
+          <div class="carousel__title">${slide.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `;
   }
 }
