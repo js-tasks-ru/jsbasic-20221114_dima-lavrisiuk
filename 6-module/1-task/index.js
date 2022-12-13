af7f76d2e2130fa0d33ee2a4065dff41cff79a98
Dima Lavrisiuk <dmitriyemigrant@gmail.com>
2022-12-13 15:45:33 +0300

    Module 6 / Task 1 Done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,60 @@
  *
  */
 export default class UserTable {
+  elem = null;
+
   constructor(rows) {
+    this.elem = document.createElement('table');
+
+    this.elem.insertAdjacentHTML('afterbegin', this.#templateTHead({
+      name: "Имя",
+      age: "Возраст",
+      salary: "Зарплата",
+      city: "Город"
+    }));
+
+    this.elem.insertAdjacentHTML('beforeend', this.#templateTBody(rows));
+
+    this.elem.addEventListener('click', this.#onButtonClick);
+  }
+
+  #onButtonClick = (event) => {
+    if(event.target.localName === 'button') {
+      event.target.closest('tr').remove();
+    }
+  };
+
+  #templateTHead(labels) {
+    return `
+      <thead>
+        <tr>
+          <th>${labels.name}</th>
+          <th>${labels.age}</th>
+          <th>${labels.salary}</th>
+          <th>${labels.city}</th>
+          <th></th>
+        </tr>
+      </thead>
+    `;
+  }
+
+  #templateTBody(rows) {
+    return `
+      <tbody>
+        ${rows.map(row => this.#templateRow(row)).join('')}
+      </tbody>
+    `;
+  }
+
+  #templateRow(row) {
+    return  `
+      <tr>
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      </tr>
+    `;
   }
 }
